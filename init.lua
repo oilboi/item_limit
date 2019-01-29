e456042084baf4a2af274f1fc3dfd5df441a71af
oilboi <47129783+oilboi@users.noreply.github.com>
2019-01-29 16:32:00 -0500

    remove debug

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 local item_limit_stack_max = 999 -- the max item limit per stack
 minetest.register_on_mods_loaded(function()
   for i,a in pairs(minetest.registered_items) do
-    print(a.name)
     minetest.override_item(a.name, {stack_max = item_limit_stack_max})
   end
 end)
